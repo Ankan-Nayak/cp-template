fd97e9a279ada848fbc962d93f98b61427077166
Ankan Nayak <ankan.nayak.1412@gmail.com>
2025-06-29 01:04:57 +0530

    pascal triangle

--- Number-Theory/pascal-triangle.cpp
+++ Number-Theory/pascal-triangle.cpp
@@ -9,3 +9,36 @@ void pascalTriangle(int n) {
     }
 }
 // table make i = n, j = r,  => ncr = table[i][j]
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+const int mod = 1e9;
+
+int nCr[1001][1001]; // n, r <= 1000
+
+void precompute() {
+    for(int n = 0; n <= 1000; n++) {
+        nCr[n][0] = 1;
+        for(int r = 1; r <= n; r++) {
+            nCr[n][r] = (nCr[n-1][r-1] + nCr[n-1][r]) % mod;
+        }
+    }
+}
+
+signed main() {
+    precompute();
+    int T;
+    cin >> T;
+    while(T--) {
+        int n;
+        cin >> n;
+        for(int r = 0; r <= n; r++) { // fixed boundary to r <= n
+            cout << nCr[n][r] << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
