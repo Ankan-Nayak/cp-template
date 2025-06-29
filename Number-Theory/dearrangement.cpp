b2187dffad4a42d634d707c87528ddb0474264a1
Ankan Nayak <ankan.nayak.1412@gmail.com>
2025-06-29 21:29:10 +0530

    dearrangement

---
+++ Number-Theory/dearrangement.cpp
@@ -0,0 +1,39 @@
+// iterative dp with mod
+#include <iostream>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+long long der[1000001];
+
+void compute_derangements(int N) {
+    der[1] = 0, der[2] = 1;
+    for (int i = 3; i <= N; i++) {
+        der[i] = ((i - 1) * (der[i - 1] + der[i - 2])) % MOD;
+    }
+}
+
+int main() {
+    int N;
+    cin >> N;
+    compute_derangements(N);
+    cout << der[N] << endl;
+    return 0;
+}
+
+
+// recursive
+#include <iostream>
+using namespace std;
+
+long long derangement(int n) {
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+    return (n - 1) * (derangement(n - 1) + derangement(n - 2));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << derangement(n) << endl;
+    return 0;
+}
