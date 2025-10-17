0ab16664335fd2c98901edcfa0cec8399aafa11f
Ankan Nayak <ankan.nayak.1412@gmail.com>
2025-10-17 18:23:07 +0530

    update

--- Graph/floyd-warshall.cpp
+++ Graph/floyd-warshall.cpp
@@ -10,8 +10,8 @@ signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);cout.tie(0);
 
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 1; j <= N; ++j) {
             if (i == j) dist[i][j] = 0;
             else dist[i][j] = inf;
         }
