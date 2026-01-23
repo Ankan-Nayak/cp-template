94458b6367e08357812fb255499d472cb2f1f508
Ankan Nayak <ankan.nayak.1412@gmail.com>
2026-01-23 16:57:27 +0530

    az

---
+++ AZ/Binary_Search/longest_inc_subseq.cpp
@@ -0,0 +1,23 @@
+int LIS(vector<int>& a) {
+    vector<int> temp;
+
+    for (int x : a) {
+        // find first element >= x
+        auto it = lower_bound(temp.begin(), temp.end(), x);
+
+        if (it == temp.end()) {
+            // x is bigger than all elements → extend LIS
+            temp.push_back(x);
+        } else {
+            // replace to keep smallest possible tail
+            *it = x;
+        }
+    }
+
+    return temp.size();
+}
+
+
+// a = [10, 9, 2, 5, 3, 7, 101, 18]
+// LIS length = 4  (2, 3, 7, 101)
+// 0(NlogN)
