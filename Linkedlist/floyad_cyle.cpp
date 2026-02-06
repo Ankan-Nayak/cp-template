c59f3db73b1afe1a92c8e9df46d3002d4d34c146
Ankan Nayak <ankan.nayak.1412@gmail.com>
2026-02-07 02:29:19 +0530

    az

--- Linkedlist/floyad_cyle.cpp
+++ Linkedlist/floyad_cyle.cpp
@@ -42,7 +42,7 @@ ListNode* detectCycle(ListNode* head) {
 
 // L + x + k₂·C = 2(L + x + k₁·C)
 // k₂·C − 2k₁·C = L + x
-// (k₂ − 2k₁) · C = L + x
+// (k₂ − 2k₁) · C = L + x......if we do fast as 3 or other steps still (k₂ − nk₁) = K'..so same thing, we don't do, becuse it take more time to match slow = fast
 // L + x = k · C
 // L + x = k·C
 // ⇒ L = k·C − x
