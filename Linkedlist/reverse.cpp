fffdd809ae7a6213b6168b785d871831825067b8
Ankan Nayak <ankan.nayak.1412@gmail.com>
2026-02-07 02:48:59 +0530

    az

--- Linkedlist/reverse.cpp
+++ Linkedlist/reverse.cpp
@@ -40,3 +40,32 @@ head.next.next.next.next = new Node(5);
  
 head = reverseList(head);
 printList(head);
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        while (curr != nullptr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+        return head;
+    }
+};
