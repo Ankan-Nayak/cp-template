59cf73268f07f2e9f75181cd37ab01ebedfce314
Ankan Nayak <ankan.nayak.1412@gmail.com>
2026-02-07 22:45:54 +0530

    az

--- Linkedlist/intersection.cpp
+++ Linkedlist/intersection.cpp
@@ -23,3 +23,33 @@ public:
 };
 
 // or use diff of length start from the max len - diff pos
+
+
+ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    //Complete the function
+    ListNode* ptr1 = headA;
+    ListNode* ptr2 = headB;
+    int countA = 0, countB = 0;
+    while (ptr1) {
+        ptr1 = ptr1->next;
+        countA += 1;
+    }
+    while (ptr2) {
+        ptr2 = ptr2->next;
+        countB += 1;
+    }
+    int diff = abs(countA - countB);
+    ptr1 = headA, ptr2 = headB;
+    if (countA > countB) {
+        while (diff--) ptr1 = ptr1->next;
+    } else {
+        while (diff--) ptr2 = ptr2->next;
+    }
+
+    while (ptr1 && ptr2) {
+        if (ptr1 == ptr2) return ptr1;
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+    return NULL;
+}
