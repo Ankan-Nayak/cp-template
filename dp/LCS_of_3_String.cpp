a616c3451e2a6305a7a669165e1c971723f3b39e
Ankan Nayak <ankan.nayak.1412@gmail.com>
2026-02-17 11:56:11 +0530

    az

--- dp/LCS_of_3_String.cpp
+++ dp/LCS_of_3_String.cpp
@@ -1,33 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std;
 
-int dp[102][102][102];
-string s1, s2, s3;
-int n1, n2, n3;
 
-int f(int i, int j, int k) {
-    if (i >= n1  ||  j >= n2  ||  k >= n3) {
-        return 0;
-    }
-    if (dp[i][j][k] != -1) return dp[i][j][k];
-    int ans = 0;
-    if (s1[i] == s2[j]  &&  s2[j] == s3[k]) {
-        ans = max(ans, 1 + f(i + 1, j + 1, k + 1));
-    }
-    return dp[i][j][k] = max({ans, f(i + 1, j, k), f(i, j + 1, k), f(i, j, k + 1)});
-}
 
-signed main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);cout.tie(0);
+/*
+        vector<vector<vector<int>>> dp(n1+1, vector<vector<int>> (n2+1, vector<int>(n3+1, 0)));
 
-    int tt;
-    cin >> tt;
-    while (tt--) {
-        cin >> s1 >> s2 >> s3;
-        n1 = s1.size(), n2 = s2.size(), n3 = s3.size();
-        memset(dp, -1, sizeof(dp));
-        cout << f(0, 0, 0) << endl;
-    }
-    return 0;
-}
+        for (int i = 1; i <= n1; ++i) {
+            for (int j = 1; j <= n2; ++j) {
+                for (int k = 1; k <= n3; ++k) {
+                    if (s1[i - 1] == s2[j - 1]  &&  s2[j - 1] == s3[k - 1]) dp[i][j][k] = max(dp[i][j][k], 1 + dp[i - 1][j - 1][k - 1]);
+                    dp[i][j][k] = max({dp[i][j][k], dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1]});
+                }
+            }
+        }
+
+        cout << dp[n1][n2][n3] << '\n';
+*/
