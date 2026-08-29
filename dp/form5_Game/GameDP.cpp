3bc4e1b111d4ef5b17ca584819f81496429d82f2
Ankan Nayak <ankannayak@Ankans-MacBook-Air.local>
2026-08-30 00:56:59 +0530

    your message

--- dp/form5_Game/GameDP.cpp
+++ dp/form5_Game/GameDP.cpp
@@ -3,6 +3,9 @@ dp(config) = W / L
 whoever comes to that pos, his fate is pre defined, calculate the predefine w/l
 now check who is coming to that pos
 
+// for most cases they follow a pattern, u can print all of them
+// directly print if (this) or that then what happens directly without using dp all together
+
 
 (state) w-> A(move) -> L(opp lose)
         w-> B(move) -> L(opp lose)
