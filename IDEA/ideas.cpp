196e1553434efb8a012cbd563118156623c21e6a
Ankan Nayak <ankannayak@Ankans-MacBook-Air.local>
2026-09-17 01:31:37 +0530

    your message

---
+++ IDEA/ideas.cpp
@@ -0,0 +1,43 @@
+/*
+stack 
+NGE, NSE, PGE, PSE
+push index in stack i, use two stack to maintain
++1,0,-1..Negative no
+*/
+
+/*
+Binary Search on Ans possible ?
+*/
+
+/*
+prefix sum
+pref Max, suff Max
+both side low, high -> which should be lesser in what low-1 or high+1
+
+2d pref sum
+differential array
+*/
+
+/*
+sparse table ?
+*/
+
+/*
+how much current index is conyributing to ans
+*/
+
+/*
+0 -1 1 -2 ... -2 ->  [-2 .... -2] is sum = 0, for mp[0] = 1 by default
+*/
+
+/*
+dp -> states, defination
+how smaller problems solve bigger one
+is the state even necessary
+
+use form1, form2, form3, form4, form5
+
+transition -> rec(i,j) to goes where
+
+base case what rec(i,j) would return to help in ans
+*/
