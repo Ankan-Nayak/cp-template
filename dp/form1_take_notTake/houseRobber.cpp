116d9d52bcf2f1e47ab5bc9149fedf120693ff60
Ankan Nayak <ankannayak@Ankans-MacBook-Air.local>
2026-09-18 02:20:30 +0530

    your message

---
+++ dp/form1_take_notTake/houseRobber.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int n;
+    vector<int> a;
+    int dp[1010][2];
+    int rec(int i, int end) {
+        if (n == 1) return a[0];
+        int e = ((end == 0) ? n-1 : n);
+        if (i >= e) return 0;
+        if(dp[i][end] != -1) {
+            return dp[i][end];
+        }
+        int ans = 0;
+        ans = max(ans, a[i] + rec(i+2, end));
+        ans = max(ans, rec(i+1, end));
+
+        return dp[i][end] = ans;
+    }
+
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        a = nums;
+        memset(dp,-1,sizeof(dp));
+        int res1 = rec(0, 0);
+        int res2 = rec(1, 1);
+        
+        return max(res1, res2);
+    }
+};
+
+
+
+
+
+class Solution {
+public:
+    int n;
+    vector<int> a;
+    int dp[1010][2];
+    int rec(int i, int end) {
+        if (n == 1) return a[0];
+        int e = ((end == 0) ? n-1 : n);
+        if (i >= e) return 0;
+        if(dp[i][end] != -1) {
+            return dp[i][end];
+        }
+        int ans = 0;
+        ans = max(ans, a[i] + rec(i+2, end));
+        ans = max(ans, rec(i+1, end));
+
+        return dp[i][end] = ans;
+    }
+
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        a = nums;
+        memset(dp,-1,sizeof(dp));
+        int res1 = rec(0, 0);
+        int res2 = rec(1, 1);
+        
+        return max(res1, res2);
+    }
+};
