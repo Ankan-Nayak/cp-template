354fe9406428e31ccaf90bacff6b1bf93b89e511
Ankan Nayak <ankannayak@Ankans-MacBook-Air.local>
2026-09-18 02:21:22 +0530

    your message

--- dp/form1_take_notTake/houseRobber.cpp
+++ dp/form1_take_notTake/houseRobber.cpp
@@ -2,6 +2,50 @@ class Solution {
 public:
     int n;
     vector<int> a;
+    int dp[1010];
+    int rec(int i) {
+        if (i >= n-1) return 0;
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        int ans = 0;
+        ans = max(ans, a[i] + rec(i+2));
+        ans = max(ans, rec(i+1));
+
+        return dp[i] = ans;
+    }
+
+    int rec2(int i) {
+        if (i >= n) return 0;
+        if(dp[i] != -1) {
+            return dp[i];
+        }
+        int ans = 0;
+        ans = max(ans, a[i] + rec2(i+2));
+        ans = max(ans, rec2(i+1));
+
+        return dp[i] = ans;
+    }
+    int rob(vector<int>& nums) {
+        n = nums.size();
+        a = nums;
+        memset(dp,-1,sizeof(dp));
+        int res1 = rec(0);
+        memset(dp,-1,sizeof(dp));
+        int res2 = rec2(1);
+        
+        if (n == 1) res2 = nums[0];
+        return max(res1, res2);
+    }
+};
+
+
+
+
+class Solution {
+public:
+    int n;
+    vector<int> a;
     int dp[1010][2];
     int rec(int i, int end) {
         if (n == 1) return a[0];
