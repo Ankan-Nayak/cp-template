bf0c1ab3a54fa508bfab59b3318ee5f294b48a0e
Ankan Nayak <ankannayak@Ankans-MacBook-Air.local>
2026-09-20 14:39:49 +0530

    your message

--- dp/Optimisation/805.split_with_same_avg_map.cpp
+++ dp/Optimisation/805.split_with_same_avg_map.cpp
@@ -164,3 +164,95 @@ public:
         return false;
     }
 };
+
+
+
+
+Think of it as 0/1 Subset Sum + count.
+
+State:
+    dp[cnt][sum] = can we choose exactly `cnt` elements
+                    with total sum `sum`?
+
+Initially:
+    dp[0][0] = true
+
+For every number x:
+    for cnt from n/2 down to 1:
+        for every sum possible in dp[cnt-1]:
+
+            newSum = sum + x
+
+            dp[cnt][newSum] = true
+
+            Check:
+                newSum / cnt == total / n
+            =>  newSum * n == total * cnt
+
+Why cnt goes backwards?
+    Because each x can be used only ONCE.
+    Going backwards prevents the current x from being
+    used again in the same iteration.
+
+Example:
+    nums = [1, 2, 3]
+
+    Initially:
+        dp[0] = {0}
+
+    Process x = 1:
+        dp[1] = {1}
+
+    Process x = 2:
+        From dp[0] = {0}:
+            dp[1] gets 2
+
+        From dp[1] = {1}:
+            dp[2] gets 3
+
+    So:
+        dp[1] = {1, 2}
+        dp[2] = {3}
+
+The `i` state from recursion disappears because
+the outer loop itself represents which elements
+have been processed:
+
+    recursive:  dp[i][cnt][sum]
+    iterative: dp[cnt][sum]
+                ↑
+             i handled by for(x : nums)
+
+
+class Solution {
+public:
+    bool splitArraySameAverage(vector<int>& nums) {
+        int n = nums.size();
+
+        int total = accumulate(nums.begin(), nums.end(), 0);
+
+        // dp[cnt] = all possible sums using exactly cnt elements
+        vector<unordered_set<int>> dp(n / 2 + 1);
+
+        dp[0].insert(0);
+
+        for (int x : nums) {
+
+            // Go backwards so x is used only once
+            for (int cnt = n / 2; cnt >= 1; cnt--) {
+
+                for (int sum : dp[cnt - 1]) {
+
+                    int newSum = sum + x;
+
+                    if (newSum * n == total * cnt)
+                        return true;
+
+                    dp[cnt].insert(newSum);
+                }
+            }
+        }
+
+        return false;
+    }
+};
