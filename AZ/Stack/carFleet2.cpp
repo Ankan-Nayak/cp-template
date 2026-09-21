4e9ae67305e7b100f7e059f999c370df7e68198f
Ankan Nayak <ankannayak@Ankans-MacBook-Air.local>
2026-09-22 00:35:42 +0530

    your message

--- AZ/Stack/carFleet2.cpp
+++ AZ/Stack/carFleet2.cpp
@@ -30,3 +30,52 @@ public:
 /*We can see that we can maintain candidate cars in this way using a stack, 
 removing cars with speed greater than or equal to current car, 
 and then removing cars which can never be first point of intersection. The first car after this process (if any) would be first point of intersection.*/
+
+
+
+
+
+//car fleet 1
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = speed.size();
+        vector<pair<int, int>> a;
+        for (int i = 0; i < n; ++i) {
+            a.push_back({position[i], speed[i]});
+        }
+        sort(a.begin(), a.end());
+        stack<int> st;
+        vector<int> ans(n+1);
+        for (int i = 0; i < n; ++i) {
+            if (!st.empty() && a[st.top()].second <= a[i].second) {
+                // skip -> ct++
+            }
+
+            while (!st.empty() && a[st.top()].second > a[i].second) {
+                long double netDist = a[i].first - a[st.top()].first;
+                long double netSpeed = a[st.top()].second - a[i].second;
+                long double time = netDist / netSpeed;
+
+                if (a[i].first + (time * a[i].second) <= target) {
+                    st.pop();
+                } else {
+                    break;
+                }
+            }
+            
+            st.push(i);
+       
+        }
+        return st.size();
+    }
+};
+
+
+/*
+0 3 5 8 10
+1 3 1 4 2
+
+cur sp = x, next sp = y (y>=x)
+unable to catch beacuse of sp and dist
+*/
